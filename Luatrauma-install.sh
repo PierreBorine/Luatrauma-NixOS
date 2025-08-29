b837082863e76fff60b8f0f6363824464434a95d
PierreBorine <pierre.borine@protonmail.com>
2025-08-29 17:26:27 +0200

    feat: don't download everytimes
    How could I do that in the first place, help

--- Luatrauma-install.sh
+++ Luatrauma-install.sh
@@ -3,9 +3,7 @@
 #! nix-shell --arg config "{ allowUnfree = true; }" -p bash cacert wget steam-run
 #! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/e2f08f4d8b3ecb5cf5c9fd9cb2d53bb3c71807da.tar.gz
 
-rm Luatrauma.AutoUpdater.linux-x64
-
-wget https://github.com/Luatrauma/Luatrauma.AutoUpdater/releases/download/latest/Luatrauma.AutoUpdater.linux-x64
+wget -N https://github.com/Luatrauma/Luatrauma.AutoUpdater/releases/download/latest/Luatrauma.AutoUpdater.linux-x64
 
 chmod +x Luatrauma.AutoUpdater.linux-x64
 
