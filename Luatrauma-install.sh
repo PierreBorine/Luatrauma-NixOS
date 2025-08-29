e5b68ef6dd1a6420e91afce9b52465d3de9a4c65
PierreBorine <pierre.borine@protonmail.com>
2025-08-29 17:39:27 +0200

    refactor: use sh instead of bash

--- Luatrauma-install.sh
+++ Luatrauma-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i bash --pure
-#! nix-shell --arg config "{ allowUnfree = true; }" -p bash cacert wget steam-run
+#! nix-shell -i sh --pure
+#! nix-shell --arg config "{ allowUnfree = true; }" -p cacert wget steam-run
 #! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/e2f08f4d8b3ecb5cf5c9fd9cb2d53bb3c71807da.tar.gz
 
 wget -N https://github.com/Luatrauma/Luatrauma.AutoUpdater/releases/download/latest/Luatrauma.AutoUpdater.linux-x64
